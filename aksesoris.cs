83e713a40dc342786c331b4f310a166af52750df
alyzaseptiaanjani <alyzaseptiaanjanii@gmail.com>
2025-09-18 11:44:37 +0700

    Update aksesoris.cs

--- aksesoris.cs
+++ aksesoris.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace tugasoop_onlineshop
 {
-    internal class aksesoris
+    internal class aksesoris : produk
     {
+        public string jenis { get; set; }
+        public aksesoris(string nama, int harga, string jenis) : base(nama, harga)
+        {
+            this.jenis = jenis;
+        }
+        public string Jenis => jenis;
+        public void displayInfo()
+        {
+            Console.WriteLine($"Nama Produk: {nama}, Harga: {harga}, Jenis: {jenis}");
+        }
     }
+    
 }
