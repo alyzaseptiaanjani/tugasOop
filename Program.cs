137a42f53d64c71590d56ff7c9f30dba48f4952b
alyzaseptiaanjani <alyzaseptiaanjanii@gmail.com>
2025-09-18 11:43:45 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace tugasoop_onlineshop
+{
+    class program
+    {
+       // main
+       produk p1 = new pakaian("rok", 70000, "m");
+       produk p2 = new pakaian("celana", 80000, "l");
+       produk p3 = new aksesoris("cincin", 50000, "emas");
+       produk p4 = new aksesoris("kalung", 60000, "perak");
+
+    }
+}
