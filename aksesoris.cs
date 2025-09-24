3d41c1a70d595502f720aa90cc5b900f79961e40
alyzaseptiaanjani <alyzaseptiaanjanii@gmail.com>
2025-09-24 07:51:55 +0700

    Update aksesoris.cs

--- aksesoris.cs
+++ aksesoris.cs
@@ -4,13 +4,16 @@ namespace tugasoop_onlineshop
 {
     internal class aksesoris : produk
     {
-        public string jenis { get; set; }
+        
         public aksesoris(string nama, int harga, string jenis) : base(nama, harga)
         {
             this.jenis = jenis;
         }
-        public string Jenis => jenis;
-        public void displayInfo()
+        public string jenis { get; set; }
+
+
+        // overriding method displayInfo
+        public override void displayInfo()
         {
             Console.WriteLine($"Nama Produk: {nama}, Harga: {harga}, Jenis: {jenis}");
         }
