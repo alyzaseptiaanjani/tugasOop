c51016ca6f0dde341640998227bacd88401db7b8
alyzaseptiaanjani <alyzaseptiaanjanii@gmail.com>
2025-09-24 07:53:29 +0700

    Update produk.cs

--- produk.cs
+++ produk.cs
@@ -5,17 +5,16 @@ namespace tugasoop_onlineshop
 {
     internal class produk
     {
-        public string nama { get; set; }
-        public int harga { get; set; }
+       
         public produk(string nama, int harga)
         {
             this.nama = nama;
             this.harga = harga;
         }
-        public string Nama => nama;
-        public double Harga => harga;
+        public string nama { get; set; }
+        public int harga { get; set; }
 
-        public void displayInfo()
+        public virtual void displayInfo()
         {
             Console.WriteLine($"Nama Produk: {nama}, Harga: {harga}");
         }
