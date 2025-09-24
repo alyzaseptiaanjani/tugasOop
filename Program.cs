e5adc9f506d9b60ee3161a1bf7e8280b3ddd0991
alyzaseptiaanjani <alyzaseptiaanjanii@gmail.com>
2025-09-24 07:49:18 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,11 +2,26 @@ namespace tugasoop_onlineshop
 {
     class program
     {
-       // main
-       produk p1 = new pakaian("rok", 70000, "m");
-       produk p2 = new pakaian("celana", 80000, "l");
-       produk p3 = new aksesoris("cincin", 50000, "emas");
-       produk p4 = new aksesoris("kalung", 60000, "perak");
+       static void Main(string[] args)
+        {
+            keranjangbelanja kerkun = new keranjangbelanja();
+
+            produk p1 = new pakaian("rok", 70000, "m", "pink");
+            produk p2 = new aksesoris("cincin", 50000, "emas");
+
+            // menambahkan produk ke keranjang belanja
+            Console.WriteLine("===== SELAMAT BERBELANJA =====");
+            Console.WriteLine("Menambahkan produk ke keranjang belanja");
+            kerkun.tambahProduk(p1);
+            kerkun.tambahProduk(p2);
+            kerkun.tambahProduk("topi", 40000);
+
+            // menampilkan isi keranjang belanja
+            kerkun.tampilkanKeranjang();
+
+
+        }
+
 
     }
 }
